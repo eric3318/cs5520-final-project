9fbbb11897aa1d87483aaf3e03649fe3b0f961f0
Han Nie <nie.han@northeastern.edu>
2024-11-12 18:10:03 -0800

    modified post component to display information

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,21 +1,57 @@
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Card } from 'react-native-paper';
 
 export default function Post({ item }) {
   return (
-    <View style={styles.container}>
-      <View style={styles.upperSection}>
-        <Image source="../assets/icon.png" />
-        <Text>{item.username}</Text>
-      </View>
-    </View>
+    <Card>
+      <Card.Content style={styles.cardContent}>
+        <View style={styles.upperSection}>
+          <View style={styles.userInfo}>
+            <Image
+              source={require('../assets/icon.png')}
+              style={{ width: 50, height: 50 }}
+            />
+            <View>
+              <Text>{item.timestamp}</Text>
+              <Text>{item.username}</Text>
+            </View>
+          </View>
+          <View style={styles.like}>
+            <FontAwesome name="heart-o" size={18} color="black" />
+            <Text>{item.numLikes}</Text>
+          </View>
+        </View>
+        <View style={styles.contentSection}>
+          <Image
+            source={require('../assets/icon.png')}
+            style={{ width: '100%', height: 200 }}
+          />
+          <Text>{item.text}</Text>
+        </View>
+      </Card.Content>
+    </Card>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  cardContent: {
+    rowGap: 10,
   },
   upperSection: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 6,
+  },
+  like: {
+    alignItems: 'center',
+  },
+  contentSection: {
+    rowGap: 6,
   },
 });
--- screens/Discovery.jsx
+++ screens/Discovery.jsx
@@ -1,5 +1,44 @@
-import { Image, View } from 'react-native';
+import { FlatList, Image, View } from 'react-native';
+import Post from '../components/Post';
 
 export default function Discovery() {
-  return <View></View>;
+  const testData = [
+    {
+      username: 'abc',
+      profileImage: '',
+      timestamp: '2023-01-01 18:00',
+      text: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      numLikes: 18,
+    },
+    {
+      username: 'abc',
+      profileImage: '',
+      timestamp: '2023-01-01 18:00',
+      text: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      numLikes: 18,
+    },
+    {
+      username: 'abc',
+      profileImage: '',
+      timestamp: '2023-01-01 18:00',
+      text: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      numLikes: 18,
+    },
+    {
+      username: 'abc',
+      profileImage: '',
+      timestamp: '2023-01-01 18:00',
+      text: 'loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem',
+      numLikes: 18,
+    },
+  ];
+
+  return (
+    <View>
+      <FlatList
+        data={testData}
+        renderItem={({ item }) => <Post item={item} />}
+      />
+    </View>
+  );
 }
