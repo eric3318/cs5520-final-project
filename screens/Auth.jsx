b917ba2b4d14a793010aad5dbccfbf8ade9818a7
Han Nie <nie.han@northeastern.edu>
2024-11-13 13:50:30 -0800

    added states for text inputs

--- screens/Auth.jsx
+++ screens/Auth.jsx
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import { useState } from 'react';
 
-const SIGNUP = 'signUp';
-const LOGIN = 'logIn';
+const SIGNUP = 'Sign Up';
+const LOGIN = 'Log In';
 
 export default function Auth() {
   const [mode, setMode] = useState(SIGNUP);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const changeMode = () => {
     if (mode === SIGNUP) {
@@ -16,14 +20,43 @@ export default function Auth() {
     setMode(SIGNUP);
   };
 
+  const authHandler = () => {
+    switch (mode) {
+      case SIGNUP:
+        signUpHandler();
+        break;
+      case LOGIN:
+        logInHandler();
+        break;
+    }
+  };
+
+  const signUpHandler = () => {};
+
+  const logInHandler = () => {};
+
   return (
     <View style={styles.container}>
-      <TextInput label="Email" />
-      {mode === SIGNUP && <TextInput label="Username" />}
-      <TextInput label="Password" />
-      {mode === SIGNUP && <TextInput label="Confirm password" />}
+      <TextInput label="Email" value={email} onChangeText={setEmail} />
+      {mode === SIGNUP && (
+        <TextInput
+          label="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+      )}
+      <TextInput label="Password" value={password} onChangeText={setPassword} />
+      {mode === SIGNUP && (
+        <TextInput
+          label="Confirm password"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      )}
       <View>
-        <Button mode="contained">Register</Button>
+        <Button mode="contained" onPress={authHandler}>
+          {mode}
+        </Button>
         <View style={styles.prompt}>
           <Text>Already have an account?</Text>
           <Button onPress={changeMode}>Log in</Button>
